109e0c624e8dc41c7f9f52e705cb8256163ee538
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2024-09-23 14:17:47 +0100

    add the final functions to the script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+import { startNewGame, hit, stand } from "./game.js";
+
+document
+  .getElementById("new-game-button")
+  .addEventListener("click", startNewGame);
+document.getElementById("hit-button").addEventListener("click", hit);
+document.getElementById("stand-button").addEventListener("click", stand);
+
+startNewGame();
