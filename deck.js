fdca61c9db96b0edf1b10159d5cb12d2c7908297
Mrst12 <66439480+Mrst12@users.noreply.github.com>
2024-09-23 13:35:09 +0100

    add functions for createDeck and drawCards

---
+++ deck.js
@@ -0,0 +1,13 @@
+const apiUrl = "https://deckofcardsapi.com/api/deck";
+
+export async function createDeck() {
+  const response = await fetch(`${apiUrl}/new/shuffle/?deck_count=1`);
+  const data = await response.json();
+  return data.deck_id;
+}
+
+export async function drawCards(deckId, count) {
+  const response = await fetch(`${apiUrl}/${deckId}/draw/?count=${count}`);
+  const data = await response.json();
+  return data.cards;
+}
